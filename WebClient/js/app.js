0aa68f82779708bb6d934cc761a57b6ab6dadba3
Ben Carr <ben.carr2@ntlworld.com>
2014-08-20 19:12:33 +0100

    Started some basic gluing together of the Ember app. Needs models.

--- WebClient/js/app.js
+++ WebClient/js/app.js
@@ -1,5 +1,17 @@
 App = Ember.Application.create();
 
+openInfoModal = function(){
+	$('.modal.info').modal('show');
+};
+$(document).on('click', '.ui.button#info', openInfoModal);
+
+openHelpModal = function(){
+	$('.modal.help').modal('show');
+}
+$(document).on('click', '.ui.button#help', openHelpModal);
+
 App.Router.map(function() {
-	this.resource('about');
+	this.resource('pub');
+	this.resource('year');
 });
+
