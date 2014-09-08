8cec24c81036a14b9e1e85c90a0172e4f567ce90
Ben Carr <ben.carr2@ntlworld.com>
2014-09-08 01:16:53 +0100

    Added a function to extract text from a given page

--- PDF_Text_Extract/extract_text_simple.py
+++ PDF_Text_Extract/extract_text_simple.py
@@ -8,7 +8,7 @@ activate_this = 'Scripts\\activate_this.py'
 exec(open(activate_this).read(), dict(__file__=activate_this))
 
 def count_pages(pdf_path):
-    identify = subprocess.check_output('identify -density 12 "%s"' % pdf_path,
+    identify = subprocess.check_output('identify -density 4 "%s"' % pdf_path,
                                        stderr=subprocess.STDOUT)
     pages = []
     for line in identify.decode().split('\n'):
@@ -19,11 +19,17 @@ def count_pages(pdf_path):
             pass
     return pages
     
+def extract_page(pdf_path, pageno):
+    base_cmd = 'Scripts\\python Scripts\\pdf2txt.py -p %d "%s"'
+    try:
+        text = subprocess.check_output(base_cmd % (pageno, pdf_path),
+                                stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError as e:
+        print(e.output.decode())
+        return
+    return text.decode()
 
-try:
-    subprocess.check_output('Scripts\\python Scripts\\pdf2txt.py',
-                            stderr=subprocess.STDOUT)
-except subprocess.CalledProcessError as e:
-    print(e.output.decode())
-
-count_pages('C:\\RealDocs\\Web\\Touch_PDF_Browser\\PDFs_Sorted\\jog\\1966\\feb\\John O Gauntlet 13.pdf')
+PDF = 'C:\\RealDocs\\Web\\Touch_PDF_Browser\\PDFs_Sorted\\jog\\1966\\feb\\John O Gauntlet 13.pdf'
+p = count_pages(PDF)
+for page in p:
+    print(extract_page(PDF, page+1))
