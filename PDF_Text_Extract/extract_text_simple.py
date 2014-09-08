55e68381354cdbce11269e8805481f7ca5773410
Ben Carr <ben.carr2@ntlworld.com>
2014-09-08 19:14:09 +0100

    Got the extraction script extracting basic text from one specific pdf Also got it inserting into solr

--- PDF_Text_Extract/extract_text_simple.py
+++ PDF_Text_Extract/extract_text_simple.py
@@ -3,6 +3,9 @@ SOLR_UPDATE_ROOT = 'http://127.0.0.1:8983/solr/update'
 
 import subprocess
 import os
+import json
+import urllib.parse
+import urllib.request
 
 activate_this = 'Scripts\\activate_this.py'
 exec(open(activate_this).read(), dict(__file__=activate_this))
@@ -29,7 +32,20 @@ def extract_page(pdf_path, pageno):
         return
     return text.decode()
 
-PDF = 'C:\\RealDocs\\Web\\Touch_PDF_Browser\\PDFs_Sorted\\jog\\1966\\feb\\John O Gauntlet 13.pdf'
+PDF = 'D:\\Touch_PDF_Browser\\PDFs_Sorted\\jog\\1966\\feb\\John O Gauntlet 13.pdf'
+#PDF = 'C:\\RealDocs\\Web\\Touch_PDF_Browser\\PDFs_Sorted\\jog\\1966\\feb\\John O Gauntlet 13.pdf'
 p = count_pages(PDF)
 for page in p:
-    print(extract_page(PDF, page+1))
+    record = {}
+    record['pub_id'] = 'jog'
+    record['issue_name'] = os.path.split(PDF)[1].replace('.pdf','')
+    record['issue_id'] = record['issue_name'] + "-%d" % page
+    record['year'] = 1966
+    record['month'] = 'feb'
+    record['page'] = page
+    record['text'] = extract_page(PDF, page+1)
+    post_json = json.dumps([record]).encode()
+    print(post_json[:500])
+    post_url = SOLR_UPDATE_ROOT + "/json?commit=true"
+    post_req = urllib.request.Request(post_url, post_json, {'Content-type':'text/json'})
+    urllib.request.urlopen(post_req)
