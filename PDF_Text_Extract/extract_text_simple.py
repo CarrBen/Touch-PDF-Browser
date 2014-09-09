030e8ffdfc5729254e77091e2120a66be69318b4
Ben Carr <ben.carr2@ntlworld.com>
2014-09-09 22:31:30 +0100

    Updated text extraction script to correctly use ProcessPool

--- PDF_Text_Extract/extract_text_simple.py
+++ PDF_Text_Extract/extract_text_simple.py
@@ -33,6 +33,7 @@ def extract_page(pdf_path, pageno):
     return text.decode()
 
 def process_pdf(PDF):
+    print(PDF)
     p = count_pages(PDF)
     split_path = PDF
     split_path, issue_name = os.path.split(split_path)
@@ -40,26 +41,33 @@ def process_pdf(PDF):
     split_path, month = os.path.split(split_path)
     split_path, year = os.path.split(split_path)
     split_path, pub = os.path.split(split_path)
-    print(issue_name,month,year,pub)
-    return
     for page in p:
         record = {}
         record['pub_id'] = pub
         record['issue_name'] = issue_name
         record['issue_id'] = record['issue_name'] + "-%d" % page
+        print(record['issue_id'])
         record['year'] = int(year)
         record['month'] = month
         record['page'] = page
         record['text'] = extract_page(PDF, page+1)
-        print(record)
         post_json = json.dumps([record]).encode()
-        print(post_json[:500])
         post_url = SOLR_UPDATE_ROOT + "/json?commit=true"
         post_req = urllib.request.Request(post_url, post_json, {'Content-type':'text/json'})
         urllib.request.urlopen(post_req)
 
 pdf_dirs = os.walk(PDF_PATH)
+pdf_list = []
 for path, dirs, files in pdf_dirs:
     for file in files:
-        if '.json' not in file:
-            process_pdf(os.path.join(os.path.abspath(path), file))
+        if '.pdf' in file:
+            pdf_list.append(os.path.join(os.path.abspath(path), file))
+
+if __name__ == '__main__':
+    print(len(pdf_list))
+    from multiprocessing import Pool
+    p = Pool(2)
+    p.map(process_pdf, pdf_list)
+    
+    ##for pdf in pdf_list:
+    ##    process_pdf(pdf)
